dcf17b2f32b44505da21645093e71c5db22c39b7
zhon <zhonnyy@163.com>
2020-07-04 10:05:15 +0800

    提交

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,10 @@
+package com.itheima.pojo;
+
+public class User {
+    private String username;
+    private String sex;
+    private String userA;
+    private String userB;
+
+    private String currentDev;
+}
